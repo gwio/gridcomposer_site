e95f3e0c617231eb023ceee47a968444611dcf10
Christian Gwiozda <c.gwiozda@gmail.com>
2016-12-01 02:19:03 +0100

    a

--- nav.php
+++ nav.php
@@ -4,7 +4,6 @@
     <ul class="w3-navbar w3-gc_white">
       <li class="navbar"><a  class="navbar"  href="index.php">HOME</a></li>
       <li class="navbar"><a  class="navbar"  href="manual.php">MANUAL</a></li>
-      <li class="navbar"><a  class="navbar"  href="manual.php">CONTACT</a></li>
     </ul>
 
   </div>
